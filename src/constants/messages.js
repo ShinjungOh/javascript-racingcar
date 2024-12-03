b45036d94482ef4f59616d30091024a95e8d35f4
ShinjungOh <tlswjd9260@gmail.com>
2024-12-03 21:43:23 +0900

    refactor: 입력 메시지 상수화

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,4 +1,8 @@
 export const MESSAGES = {
+  input: {
+    carNames: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
+    tryCount: '시도할 횟수는 몇 회인가요?\n',
+  },
   error: {
     isCarNameOnlyOne: '[ERROR] 자동차 이름은 2대 이상 입력해야 합니다.',
     carNameLengthOver: '[ERROR] 자동차 이름의 길이가 잘못되었습니다.',
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -3,7 +3,7 @@ import { MESSAGES } from "../constants/messages.js";
 
 class InputView {
   static async readLineCarNames() {
-    const input = await MissionUtils.Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+    const input = await MissionUtils.Console.readLineAsync(MESSAGES.input.carNames);
     const carNames = input.toString().split(',');
 
     this.#validateCarNames(carNames);
@@ -30,7 +30,7 @@ class InputView {
   }
 
   static async readLineCount() {
-    const input = await MissionUtils.Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    const input = await MissionUtils.Console.readLineAsync(MESSAGES.input.tryCount);
     const parseInput = parseInt(input, 10);
 
     this.#validateCount(parseInput);
